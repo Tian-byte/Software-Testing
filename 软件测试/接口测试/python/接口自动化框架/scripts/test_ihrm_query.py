55efa762f186f280640439feb7a58cadaa9a5736
点点 <102277652+1474416640@users.noreply.github.com>
2024-09-04 16:03:53 +0800

    tian

--- 软件测试/接口测试/python/接口自动化框架/scripts/test_ihrm_query.py
+++ 软件测试/接口测试/python/接口自动化框架/scripts/test_ihrm_query.py
@@ -1,6 +1,7 @@
 import unittest
 
 from api.ihrm_emp_curd import IhrmEmpCURD
+from common.db_util import DBUtil
 from common.get_header import get_header
 
 
@@ -12,6 +13,13 @@ class TestEmpQuery(unittest.TestCase):
     def setUpClass(cls) -> None:
         cls.header = get_header()
 
+    def setUp(self) -> None:
+        insert_sql = "insert into bs_user(id,mobile,username) values('11232456452635',15691075769,'张三')"
+        DBUtil.uid_db(insert_sql)
+
+    def tearDown(self) -> None:
+        delete_sql = "delete from bs_user where id ='11232456452635';"
+        DBUtil.uid_db(delete_sql)
     def test01_query_emp(self):
-        resp = IhrmEmpCURD.query_emp("1066370498633486336", self.header)
+        resp = IhrmEmpCURD.query_emp("11232456452635", self.header)
         print(resp.json())
